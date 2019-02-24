fc74aee24ea3961470fdb92db9b012bf8b6666ce
Alex Kellner <alexander.kellner@in2code.de>
2019-02-24 13:51:27 +0100

    [TASK] Update requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,13 +12,13 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'lux',
     'description' => 'Living User Experience - LUX - the marketing automation tool for TYPO3.',
     'category' => 'plugin',
-    'version' => '1.24.0',
+    'version' => '2.0.0',
     'author' => 'Alex Kellner',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '8.7.0-9.5.99',
             'php' => '7.0.0-7.99.99'
         ],
         'conflicts' => [],
