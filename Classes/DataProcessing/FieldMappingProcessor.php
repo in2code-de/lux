98f857ffac4242c7d5abaea1761657a2c82c2218
Alex Kellner <alexander.kellner@in2code.de>
2021-02-26 11:32:13 +0100

    [TASK] Small cleanup

--- Classes/DataProcessing/FieldMappingProcessor.php
+++ Classes/DataProcessing/FieldMappingProcessor.php
@@ -3,6 +3,7 @@ namespace In2code\Lux\DataProcessing;
 
 use In2code\Lux\Utility\ObjectUtility;
 use TYPO3\CMS\Core\TypoScript\TypoScriptService;
+use TYPO3\CMS\Extbase\Object\Exception;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 
@@ -11,13 +12,13 @@ use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
  */
 class FieldMappingProcessor implements DataProcessorInterface
 {
-
     /**
      * @param ContentObjectRenderer $cObj The data of the content element or page
      * @param array $contentObjectConfiguration The configuration of Content Object
      * @param array $processorConfiguration The configuration of this processor
      * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
      * @return array the processed data as key/value store
+     * @throws Exception
      */
     public function process(
         ContentObjectRenderer $cObj,
@@ -33,6 +34,7 @@ class FieldMappingProcessor implements DataProcessorInterface
     /**
      * @param array $contentObjectConfiguration
      * @return mixed
+     * @throws Exception
      */
     protected function getFieldMappingSettings(array $contentObjectConfiguration): array
     {
