91bd213331c73e15ea06b0416c2c257cc40271c7
Alex Kellner <alexander.kellner@in2code.de>
2021-05-01 14:27:49 +0200

    [FEATURE] Add two viewhelpers that can be used in Luxenterprise
    I tend to store the viewhelpers in lux even if they are used in luxenterprise only at the beginning. It's easier to store all VH in one place at one hand and on the other hand, they can be simply used also in lux.

---
+++ Classes/ViewHelpers/Visitor/GetAnyPropertyViewHelper.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+namespace In2code\Lux\ViewHelpers\Visitor;
+
+use In2code\Lux\Domain\Model\Visitor;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class GetAnyPropertyViewHelper
+ */
+class GetAnyPropertyViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('visitor', Visitor::class, 'visitor', true);
+        $this->registerArgument('property', 'string', 'any property name you want to get from the visitor', true);
+    }
+
+    /**
+     * @return string
+     */
+    public function render(): string
+    {
+        /** @var Visitor $visitor */
+        $visitor = $this->arguments['visitor'];
+        return (string)$visitor->getAnyPropertyByName($this->arguments['property']);
+    }
+}
