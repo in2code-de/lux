eb4d9eec1969be0bbd7e6aca9ddaf52a4822b61a
Alex Kellner <alexander.kellner@in2code.de>
2021-06-07 15:50:05 +0200

    [FEATURE] Performance update for lead list view
    It turns out that server with low memory_limit and with visitors with a lot of page visits could cause problems in lead list module. The lazy loading of visitor.pagevisits did not work here because this 3 places are forced a relation to pagevisits. We introduced viewhelper with sql queries (over repository) here for a better performance and now lazy loading works.

---
+++ Classes/ViewHelpers/Lead/GetDateOfLatestPageVisitAndPageViewHelper.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+namespace In2code\Lux\ViewHelpers\Lead;
+
+use Doctrine\DBAL\Exception;
+use In2code\Lux\Domain\Model\Visitor;
+use In2code\Lux\Domain\Repository\PagevisitRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class GetDateOfLatestPageVisitAndPageViewHelper
+ */
+class GetDateOfLatestPageVisitAndPageViewHelper extends AbstractViewHelper
+{
+    /**
+     * @var PagevisitRepository|null
+     */
+    protected $pagevisitRepository = null;
+
+    /**
+     * GetDateOfLatestPageVisitViewHelper constructor.
+     * @param PagevisitRepository|null $pagevisitRepository
+     */
+    public function __construct(PagevisitRepository $pagevisitRepository = null)
+    {
+        $this->pagevisitRepository = $pagevisitRepository ?: GeneralUtility::makeInstance(PagevisitRepository::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('visitor', Visitor::class, 'visitor', true);
+        $this->registerArgument('pageIdentifier', 'int', 'pages.uid', true);
+    }
+
+    /**
+     * @return \DateTime|null
+     * @throws Exception
+     */
+    public function render(): ?\DateTime
+    {
+        return $this->pagevisitRepository->findLatestDateByVisitorAndPageIdentifier(
+            $this->arguments['visitor'],
+            $this->arguments['pageIdentifier']
+        );
+    }
+}
