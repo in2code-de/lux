788bd7892551075af36f1aa1558524c3674fad5e
Alexander Kellner <alexander.kellner@in2code.de>
2021-08-12 16:04:34 +0200

    [BUGFIX] Don't show note to luxenterprise in email4link backend lightbox if it is installed

--- Resources/Public/JavaScript/Static/CkEditorPlugins/luxEmail4Link/dialogs/dialog.js
+++ Resources/Public/JavaScript/Static/CkEditorPlugins/luxEmail4Link/dialogs/dialog.js
@@ -1,87 +1,99 @@
-CKEDITOR.dialog.add('luxEmail4LinkDialog', function(editor)
-{
-	/**
-	 * pendant to jQuery function "closest"
-	 *
-	 * @param elementType
-	 * @param currentSelection
-	 * @returns {*}
-	 */
-	var getParentElement = function(elementType, currentSelection) {
-		var parentElements = currentSelection.getParents();
-		for (var i = 0; i < parentElements.length; i++) {
-			if (parentElements[i].getName() === elementType) {
-				return parentElements[i];
-			}
-		}
-		return null;
-	};
+CKEDITOR.dialog.add('luxEmail4LinkDialog', function (editor) {
+  /**
+   * pendant to jQuery function "closest"
+   *
+   * @param elementType
+   * @param currentSelection
+   * @returns {*}
+   */
+  var getParentElement = function (elementType, currentSelection) {
+    var parentElements = currentSelection.getParents();
+    for (var i = 0; i < parentElements.length; i++) {
+      if (parentElements[i].getName() === elementType) {
+        return parentElements[i];
+      }
+    }
+    return null;
+  };
 
-	return {
-		title: editor.lang.luxEmail4Link.dialog,
-		minWidth: 800,
-		minHeight: 500,
-		contents: [
-			{
-				id: 'general',
-				label: 'Settings',
-				elements: [
-					{
-						type: 'text',
-						id: 'title',
-						label: editor.lang.luxEmail4Link.dialogTitle,
-						validate: CKEDITOR.dialog.validate.notEmpty(editor.lang.luxEmail4Link.validationNotEmpty),
-						required: true,
-						commit: function(data) {
-							data.title = this.getValue();
-						}
-					},
-					{
-						type: 'textarea',
-						id: 'text',
-						minHeight: 300,
-						label: editor.lang.luxEmail4Link.dialogText,
-						commit: function(data) {
-							data.text = this.getValue();
-						}
-					},
-					{
-						type: 'checkbox',
-						id: 'sendEmail',
-						label: editor.lang.luxEmail4Link.dialogSendEmail,
-						commit: function(data) {
-							data.sendEmail = this.getValue();
-						}
-					},
-					{
-						type: 'html',
-						html : '<p>' + editor.lang.luxEmail4Link.dialogExplanation + '</p>'
-					},
-					{
-						type: 'html',
-						html : '<p><a href="https://www.in2code.de/produkte/lux-typo3-marketing-automation/" target="_blank" rel="noopener" style="font-weight:bold;text-decoration:underline;cursor:pointer;">' + editor.lang.luxEmail4Link.dialogPoweredBy + '</a></p>'
-					}
-				]
-			}
-		],
+  /**
+   * @returns {boolean}
+   */
+  var isLuxenterpriseExisting = function() {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', '/typo3conf/ext/luxenterprise/Resources/Public/Icons/lux.svg', false);
+    http.send();
+    return http.status !== 404;
+  }
 
-		// If editor submits the dialog
-		onOk: function() {
-			var data = {};
-			this.commitContent(data);
-			var parent = getParentElement('a', editor.getSelection().getStartElement());
-			parent.setAttribute('data-lux-email4link-title', data.title);
-			parent.setAttribute('data-lux-email4link-text', data.text);
-			parent.setAttribute('data-lux-email4link-sendEmail', data.sendEmail);
-		},
+  return {
+    title: editor.lang.luxEmail4Link.dialog,
+    minWidth: 800,
+    minHeight: 500,
+    contents: [
+      {
+        id: 'general',
+        label: 'Settings',
+        elements: [
+          {
+            type: 'text',
+            id: 'title',
+            label: editor.lang.luxEmail4Link.dialogTitle,
+            validate: CKEDITOR.dialog.validate.notEmpty(editor.lang.luxEmail4Link.validationNotEmpty),
+            required: true,
+            commit: function (data) {
+              data.title = this.getValue();
+            }
+          },
+          {
+            type: 'textarea',
+            id: 'text',
+            minHeight: 300,
+            label: editor.lang.luxEmail4Link.dialogText,
+            commit: function (data) {
+              data.text = this.getValue();
+            }
+          },
+          {
+            type: 'checkbox',
+            id: 'sendEmail',
+            label: editor.lang.luxEmail4Link.dialogSendEmail,
+            commit: function (data) {
+              data.sendEmail = this.getValue();
+            }
+          },
+          {
+            type: 'html',
+            html: '<p>' + editor.lang.luxEmail4Link.dialogExplanation + '</p>'
+          },
+          {
+            type: 'html',
+            html: '<p id="poweredbylux"><a href="https://www.in2code.de/produkte/lux-typo3-marketing-automation/" target="_blank" rel="noopener" style="font-weight:bold;text-decoration:underline;cursor:pointer;">' + editor.lang.luxEmail4Link.dialogPoweredBy + '</a></p>'
+          }
+        ]
+      }
+    ],
 
-		// On opening dialog box
-		onShow: function() {
-			var parent = getParentElement('a', editor.getSelection().getStartElement());
-			this.setValueOf('general', 'title', parent.getAttribute('data-lux-email4link-title') || '');
-			this.setValueOf('general', 'text', parent.getAttribute('data-lux-email4link-text') || '');
-			var sendEmailStatus = parent.getAttribute('data-lux-email4link-sendEmail') || '';
-			this.setValueOf('general', 'sendEmail', sendEmailStatus === 'true');
-		}
-	};
+    // If editor submits the dialog
+    onOk: function () {
+      var data = {};
+      this.commitContent(data);
+      var parent = getParentElement('a', editor.getSelection().getStartElement());
+      parent.setAttribute('data-lux-email4link-title', data.title);
+      parent.setAttribute('data-lux-email4link-text', data.text);
+      parent.setAttribute('data-lux-email4link-sendEmail', data.sendEmail);
+    },
+
+    // On opening dialog box
+    onShow: function () {
+      var parent = getParentElement('a', editor.getSelection().getStartElement());
+      this.setValueOf('general', 'title', parent.getAttribute('data-lux-email4link-title') || '');
+      this.setValueOf('general', 'text', parent.getAttribute('data-lux-email4link-text') || '');
+      var sendEmailStatus = parent.getAttribute('data-lux-email4link-sendEmail') || '';
+      this.setValueOf('general', 'sendEmail', sendEmailStatus === 'true');
+      if (isLuxenterpriseExisting()) {
+        document.getElementById('poweredbylux').style.display = 'none';
+      }
+    }
+  };
 });
