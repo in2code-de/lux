3df90e8d8045f2d4e7890b125a8582d82310868c
Alexander Kellner <alexander.kellner@in2code.de>
2022-06-07 16:25:05 +0200

    [BUGFIX] dashboard should be under suggested configuration
    for a correct loading order, and of course the dashboard is supported

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,9 +14,9 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '10.4.0-11.5.99',
             'php' => '7.2.0-8.0.99',
         ],
-        'conflicts' => [
+        'conflicts' => [],
+        'suggests' => [
             'dashboard' => '0.0.0-0.0.0',
         ],
-        'suggests' => [],
     ]
 ];
