2953a0cdb3bd42cbccfb245fba777761b0fbc0b0
Alexander Kellner <alexander.kellner@in2code.de>
2022-12-21 14:04:28 +0100

    [TASK] Add backend modules for TYPO3 12

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,131 @@
+<?php
+
+$configuration = [];
+
+if (\In2code\Lux\Utility\ConfigurationUtility::isAnalysisModuleDisabled() === false
+    || \In2code\Lux\Utility\ConfigurationUtility::isWorkflowModuleDisabled() === false) {
+    $configuration['lux_module'] = [
+        'labels' => 'LLL:EXT:lux/Resources/Private/Language/locallang_mod.xlf',
+        'iconIdentifier' => 'extension-lux-module',
+    ];
+}
+
+if (\In2code\Lux\Utility\ConfigurationUtility::isAnalysisModuleDisabled() === false) {
+    $configuration['lux_Analysis'] = [
+        'parent' => 'lux_module',
+        'position' => [],
+        'access' => 'user,group',
+        'iconIdentifier' => 'extension-lux-module-analysis',
+        'path' => '/module/lux/Analysis',
+        'labels' => 'LLL:EXT:lux/Resources/Private/Language/locallang_mod_analysis.xlf',
+        'extensionName' => 'Lux',
+        'controllerActions' => [
+            \In2code\Lux\Controller\AnalysisController::class => [
+                'dashboard',
+                'content',
+                'news',
+                'linkListener',
+                'search',
+                'deleteLinkListener',
+                'detailPage',
+                'detailNews',
+                'detailSearch',
+                'detailDownload',
+                'detailLinkListener',
+                'resetFilter',
+                'utm',
+            ],
+            \In2code\Lux\Controller\LeadController::class => [
+                'dashboard',
+                'list',
+                'detail',
+                'downloadCsv',
+                'remove',
+                'deactivate',
+                'resetFilter',
+            ],
+            \In2code\Lux\Controller\GeneralController::class => [
+                'information',
+            ],
+        ],
+    ];
+}
+
+if (\In2code\Lux\Utility\ConfigurationUtility::isLeadModuleDisabled() === false) {
+    $configuration['lux_Lead'] = [
+        'parent' => 'lux_module',
+        'position' => [],
+        'access' => 'user,group',
+        'iconIdentifier' => 'extension-lux-module-lead',
+        'path' => '/module/lux/Lead',
+        'labels' => 'LLL:EXT:lux/Resources/Private/Language/locallang_mod_lead.xlf',
+        'extensionName' => 'Lux',
+        'controllerActions' => [
+            \In2code\Lux\Controller\LeadController::class => [
+                'dashboard',
+                'list',
+                'detail',
+                'downloadCsv',
+                'remove',
+                'deactivate',
+                'resetFilter',
+            ],
+            \In2code\Lux\Controller\AnalysisController::class => [
+                'dashboard',
+                'content',
+                'linkClicks',
+                'detailPage',
+                'detailDownload',
+                'resetFilter',
+            ],
+            \In2code\Lux\Controller\GeneralController::class => [
+                'information',
+            ],
+        ],
+    ];
+}
+
+if (\In2code\Lux\Utility\ConfigurationUtility::isWorkflowModuleDisabled() === false) {
+    $configuration['lux_Workflow'] = [
+        'parent' => 'lux_module',
+        'position' => [],
+        'access' => 'user,group',
+        'iconIdentifier' => 'extension-lux-module-workflow',
+        'path' => '/module/lux/Workflow',
+        'labels' => 'LLL:EXT:lux/Resources/Private/Language/locallang_mod_workflow.xlf',
+        'extensionName' => 'Lux',
+        'controllerActions' => [
+            \In2code\Lux\Controller\WorkflowController::class => [
+                'list',
+                'new',
+                'create',
+                'edit',
+                'update',
+                'delete',
+                'disable',
+                'enable',
+                'resetFilter',
+            ],
+            \In2code\Lux\Controller\AbTestingController::class => [
+                'list',
+            ],
+            \In2code\Lux\Controller\ShortenerController::class => [
+                'list',
+                'delete',
+                'detail',
+                'resetFilter',
+                'qr',
+            ],
+            \In2code\Lux\Controller\UtmGeneratorController::class => [
+                'list',
+                'delete',
+                'resetFilter',
+            ],
+            \In2code\Lux\Controller\GeneralController::class => [
+                'information',
+            ],
+        ],
+    ];
+}
+
+return $configuration;
--- ext_tables.php
+++ ext_tables.php
@@ -27,12 +27,28 @@ call_user_func(
             \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
             ['source' => 'EXT:lux/Resources/Public/Icons/Extension.svg']
         );
+        $iconRegistry->registerIcon(
+            'extension-lux-module-analysis',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:lux/Resources/Public/Icons/lux_module_analysis.svg']
+        );
+        $iconRegistry->registerIcon(
+            'extension-lux-module-lead',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:lux/Resources/Public/Icons/lux_module_lead.svg']
+        );
+        $iconRegistry->registerIcon(
+            'extension-lux-module-workflow',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:lux/Resources/Public/Icons/lux_module_workflow.svg']
+        );
 
         /**
          * Include Modules
          */
         // Add Main module "LUX".
         // Acces to a main module is implicit, as soon as a user has access to at least one of its submodules.
+        // Todo: Can be removed, if TYPO3 11 support is dropped
         if (\In2code\Lux\Utility\ConfigurationUtility::isAnalysisModuleDisabled() === false
             || \In2code\Lux\Utility\ConfigurationUtility::isWorkflowModuleDisabled() === false) {
             \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
@@ -48,6 +64,7 @@ call_user_func(
             );
         }
         // Add module for analysis
+        // Todo: Can be removed, if TYPO3 11 support is dropped
         if (\In2code\Lux\Utility\ConfigurationUtility::isAnalysisModuleDisabled() === false) {
             \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'Lux',
@@ -70,6 +87,7 @@ call_user_func(
             );
         }
         // Add module for leads
+        // Todo: Can be removed, if TYPO3 11 support is dropped
         if (\In2code\Lux\Utility\ConfigurationUtility::isLeadModuleDisabled() === false) {
             \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'Lux',
@@ -91,6 +109,7 @@ call_user_func(
             );
         }
         // Add module for campaigns
+        // Todo: Can be removed, if TYPO3 11 support is dropped
         if (\In2code\Lux\Utility\ConfigurationUtility::isWorkflowModuleDisabled() === false) {
             \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
                 'Lux',
