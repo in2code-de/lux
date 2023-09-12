a7b233d777c94771474e6ad82e5e13ef144b2cb0
Alexander Kellner <alexander.kellner@in2code.de>
2023-09-12 16:26:04 +0200

    [FEATURE] Load pagevisits in Analysis/Dashboard via AJAX

---
+++ Classes/Backend/Units/AbstractUnit.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\Lux\Backend\Units;
+
+use In2code\Lux\Domain\Cache\CacheLayer;
+use In2code\Lux\Domain\Model\Transfer\FilterDto;
+use In2code\Lux\Utility\BackendUtility;
+use In2code\Lux\Utility\ObjectUtility;
+use In2code\Lux\Utility\StringUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+abstract class AbstractUnit
+{
+    protected string $templateRootPath = 'EXT:lux/Resources/Private/Templates/Backend/Units';
+    protected string $partialRootPath = 'EXT:lux/Resources/Private/Partials';
+    protected string $classPrefix = 'In2code\Lux\Backend\Units\\';
+
+    /**
+     * Optional string for cacheLayer
+     *
+     * @var string
+     */
+    protected string $cacheLayerClass = '';
+
+    /**
+     * Optional string for cacheLayer
+     *
+     * @var string
+     */
+    protected string $cacheLayerFunction = '';
+
+    /**
+     * Optional string for filter from session
+     *
+     * @var string
+     */
+    protected string $filterClass = '';
+
+    /**
+     * Optional string for filter from session
+     *
+     * @var string
+     */
+    protected string $filterFunction = '';
+
+    public function get(): string
+    {
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplateRootPaths([$this->templateRootPath]);
+        $view->setPartialRootPaths([$this->partialRootPath]);
+        $view->setTemplate($this->getTemplatePath());
+        $view->assignMultiple([
+                'cacheLayerClass' => $this->cacheLayerClass,
+                'cacheLayerFunction' => $this->cacheLayerFunction,
+                'filterClass' => $this->filterClass,
+                'filterFunction' => $this->filterFunction,
+            ] + $this->assignAdditionalVariables());
+        $this->assignCacheLayer($view);
+        $this->assignFilter($view);
+        return $view->render();
+    }
+
+    protected function assignAdditionalVariables(): array
+    {
+        return [];
+    }
+
+    protected function assignCacheLayer(StandaloneView $view): void
+    {
+        if ($this->cacheLayerClass !== '' && $this->cacheLayerFunction !== '') {
+            $cacheLayer = GeneralUtility::makeInstance(CacheLayer::class);
+            $cacheLayer->initialize($this->cacheLayerClass, $this->cacheLayerFunction);
+            $view->assign('cacheLayer', $cacheLayer);
+        }
+    }
+
+    protected function assignFilter(StandaloneView $view): void
+    {
+        $filter = ObjectUtility::getFilterDto();
+        if ($this->filterClass !== '' && $this->filterFunction !== '') {
+            $filterFromSession = BackendUtility::getSessionValue('filter', $this->filterFunction, $this->filterClass);
+            if (is_a($filterFromSession, FilterDto::class)) {
+                $filter = $filterFromSession;
+            }
+        }
+        $view->assign('filter', $filter);
+    }
+
+    protected function getTemplatePath(): string
+    {
+        $path = StringUtility::removeStringPrefix(static::class, $this->classPrefix);
+        return str_replace('\\', '/', $path);
+    }
+}
---
+++ Classes/Backend/Units/Analysis/Dashboard/Pagevisits.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\Lux\Backend\Units\Analysis\Dashboard;
+
+use In2code\Lux\Backend\Units\AbstractUnit;
+use In2code\Lux\Backend\Units\UnitInterface;
+use In2code\Lux\Controller\AnalysisController;
+use In2code\Lux\Domain\DataProvider\PagevisistsDataProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Pagevisits extends AbstractUnit implements UnitInterface
+{
+    protected string $cacheLayerClass = AnalysisController::class;
+    protected string $cacheLayerFunction = 'dashboardAction';
+    protected string $filterClass = 'Analysis';
+    protected string $filterFunction = 'dashboard';
+
+    protected function assignAdditionalVariables(): array
+    {
+        return [
+            'numberOfVisitorsData' => GeneralUtility::makeInstance(PagevisistsDataProvider::class),
+        ];
+    }
+}
