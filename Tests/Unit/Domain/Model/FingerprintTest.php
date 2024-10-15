884e832005aae50eb105c6d952193bb65d795723
Andreas Nedbal <andreas.nedbal@in2code.de>
2024-10-15 14:36:42 +0200

    [TASK] Add tests for Fingerprint model class
    Related: https://projekte.in2code.de/issues/67221

---
+++ Tests/Unit/Domain/Model/FingerprintTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace In2code\Lux\Tests\Unit\Domain\Model;
+
+use In2code\Lux\Domain\Model\Fingerprint;
+use In2code\Lux\Tests\Helper\TestingHelper;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * @coversDefaultClass \In2code\Lux\Domain\Model\Fingerprint
+ */
+class FingerprintTest extends UnitTestCase
+{
+    protected bool $resetSingletonInstances = true;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        TestingHelper::setDefaultConstants();
+    }
+
+    /**
+     * @return void
+     * @covers ::setValue
+     */
+    public function testFingerprintType(): void
+    {
+        $fingerprint = new Fingerprint();
+
+        $fingerprint->setValue(random_bytes(32));
+        self::assertEquals($fingerprint->getType(), Fingerprint::TYPE_FINGERPRINT);
+
+        $fingerprint->setValue(random_bytes(33));
+        self::assertEquals($fingerprint->getType(), Fingerprint::TYPE_STORAGE);
+    }
+
+    /**
+     * @return void
+     * @covers ::setValue
+     */
+    public function testAssertSameHashesWithSameIps(): void
+    {
+        $fingerprint = new Fingerprint();
+        $identifier = bin2hex(random_bytes(16));
+
+        $fingerprint->setValue($identifier);
+        $value1 = $fingerprint->getValue();
+
+        $fingerprint->setValue($identifier);
+        $value2 = $fingerprint->getValue();
+
+        self::assertEquals($value1, $value2);
+    }
+
+    /**
+     * @return void
+     * @covers ::setValue
+     */
+    public function testAssertDifferentHashesWithDifferentIps(): void
+    {
+        $fingerprint = new Fingerprint();
+        $identifier = bin2hex(random_bytes(16));
+
+        GeneralUtility::setIndpEnv('REMOTE_ADDR', '192.168.178.1');
+        $fingerprint->setValue($identifier);
+
+        $value1 = $fingerprint->getValue();
+
+        GeneralUtility::setIndpEnv('REMOTE_ADDR', '192.168.178.16');
+        $fingerprint->setValue($identifier);
+
+        $value2 = $fingerprint->getValue();
+
+        self::assertNotEquals($value1, $value2);
+    }
+}
