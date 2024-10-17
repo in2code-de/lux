b340a9d8e95c89ae491cd066612418276b37eb0f
Andreas Nedbal <andreas.nedbal@in2code.de>
2024-10-17 15:49:32 +0200

    [BUGFIX] Fix wrong exclude path in JavaScriptModules configuration
    Related: https://projekte.in2code.de/issues/67169

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -8,7 +8,7 @@ return [
         '@in2code/lux/' => [
             'path' => 'EXT:lux/Resources/Public/JavaScript/Lux/',
             'exclude' => [
-                'EXT:backend/Resources/Public/JavaScript/modern/',
+                'EXT:lux/Resources/Public/JavaScript/modern/',
             ],
         ],
         '@in2code/lux/vendor/chartjs.js' => 'EXT:lux/Resources/Public/JavaScript/Vendor/Chart.min.js',
