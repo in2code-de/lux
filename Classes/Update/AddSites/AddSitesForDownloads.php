6076093b20b716684272d3cc432c1019a9463bd4
Marc Bastian Heinrichs <typo3@mbh-software.de>
2024-10-18 13:53:08 +0200

    [BUGFIX] Support large tables in addSitesUpgradeWizard
    * removes duplicate code
    * do not load the complete tables in memory
    * less queries using group by pages
    * delete records without relation to page
    * update only records with empty site
    
    Resolves: #62

--- Classes/Update/AddSites/AddSitesForDownloads.php
+++ Classes/Update/AddSites/AddSitesForDownloads.php
@@ -9,17 +9,5 @@ use In2code\Lux\Utility\DatabaseUtility;
 
 class AddSitesForDownloads extends AbstractSiteUpgrade
 {
-    public function run(): void
-    {
-        $connection = DatabaseUtility::getConnectionForTable(Download::TABLE_NAME);
-        $records = $connection
-            ->executeQuery('select * from ' . Download::TABLE_NAME . ' where deleted=0')
-            ->fetchAllAssociative();
-        foreach ($records as $record) {
-            $siteIdentifier = $this->getSiteIdentifierFromPage($record['page']);
-            $connection->executeQuery(
-                'update ' . Download::TABLE_NAME . ' set site = "' . $siteIdentifier . '" where uid=' . $record['uid']
-            );
-        }
-    }
+    protected const TABLE_NAME = Download::TABLE_NAME;
 }
