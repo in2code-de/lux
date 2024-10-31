9dc853c7d6b40949b230f8058aacaf7d53634bee
Alexander Kellner <alexander.kellner@in2code.de>
2024-10-31 15:07:00 +0100

    [TASK] Rename evaluateCondition() to verdict() for Condition VH in TYPO3 13
    It seems that verdict() now replaces the old function

--- Classes/ViewHelpers/Condition/IsLuxenterpriseExtensionActivatedViewHelper.php
+++ Classes/ViewHelpers/Condition/IsLuxenterpriseExtensionActivatedViewHelper.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace In2code\Lux\ViewHelpers\Condition;
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
 
 class IsLuxenterpriseExtensionActivatedViewHelper extends AbstractConditionViewHelper
 {
-    protected static function evaluateCondition($arguments = null): bool
+    public static function verdict(array $arguments, RenderingContextInterface $renderingContext)
     {
         return ExtensionManagementUtility::isLoaded('luxenterprise');
     }
