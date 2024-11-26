d310905273c7ce2517e4439f73099f88e69d773c
Alexander Kellner <alexander.kellner@in2code.de>
2024-11-26 14:59:50 +0100

    [BUGFIX] Update requirements also in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
