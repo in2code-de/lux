1b075fc03520825415a01e56712dec1d216b5762
Alexander Kellner <alexander.kellner@in2code.de>
2026-01-30 14:54:26 +0100

    [BUGFIX] Add "hash" as valid GET parameter to be excluded from chash check
    Because /redirect.php?hash=abc... is a valid URL before a second redirect happens in shorteners

--- Classes/Utility/CacheHashUtility.php
+++ Classes/Utility/CacheHashUtility.php
@@ -11,32 +11,34 @@ class CacheHashUtility
      * @var array|string[]
      */
     protected static array $excludedVariables = [
-        'tx_lux_fe[dispatchAction]',
-        'tx_lux_fe[identificator]',
-        'tx_lux_fe[arguments][pageUid]',
-        'tx_lux_fe[arguments][languageUid]',
-        'tx_lux_fe[arguments][referrer]',
-        'tx_lux_fe[arguments][currentUrl]',
-        'tx_lux_fe[arguments][newsUid]',
+        'hash', // redirect hash for JavaScript (part of shortener before second redirect)
+        'luxContent',
+        'lux[doubleoptin][hash]',
+        'tx_lux_email4link[arguments][dummy]', // Placeholder as reminder for a dummy in email4link
+        'tx_lux_email4link[href]',
+        'tx_lux_email4link[text]',
+        'tx_lux_email4link[title]',
         'tx_lux_fe[arguments][abPageVisitIdentifier]',
         'tx_lux_fe[arguments][abTestingPage]',
+        'tx_lux_fe[arguments][currentUrl]',
         'tx_lux_fe[arguments][href]',
-        'tx_lux_fe[arguments][linklistenerIdentifier]',
-        'tx_lux_fe[arguments][redirectHash]',
-        'tx_lux_fe[arguments][sendEmail]',
-        'tx_lux_fe[arguments][values]',
         'tx_lux_fe[arguments][key]',
-        'tx_lux_fe[arguments][value]',
+        'tx_lux_fe[arguments][languageUid]',
+        'tx_lux_fe[arguments][linklistenerIdentifier]',
+        'tx_lux_fe[arguments][newsUid]',
+        'tx_lux_fe[arguments][pageUid]',
         'tx_lux_fe[arguments][parameter]',
-        'tx_lux_email4link[title]',
-        'tx_lux_email4link[text]',
-        'tx_lux_email4link[href]',
-        'tx_lux_email4link[arguments][dummy]', // Placeholder as reminder for a dummy in email4link
-        'luxContent',
+        'tx_lux_fe[arguments][redirectHash]', // redirect hash for shortener redirect
+        'tx_lux_fe[arguments][referrer]',
+        'tx_lux_fe[arguments][sendEmail]',
+        'tx_lux_fe[arguments][value]',
+        'tx_lux_fe[arguments][values]',
+        'tx_lux_fe[dispatchAction]',
+        'tx_lux_fe[identificator]',
+        'tx_luxenterprise_api[arguments]',
         'tx_luxenterprise_cc[contentUid]',
         'tx_luxenterprise_cc[identificator]',
         'tx_luxenterprise_tr[typolink]',
-        'tx_luxenterprise_api[arguments]',
     ];
 
     public static function addLuxArgumentsToExcludedVariables(): void
