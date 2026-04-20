f062afc46cfc9769ad33b2977f14bd7d0f7cde73
Alexander Kellner <alexander.kellner@in2code.de>
2026-04-20 17:02:15 +0200

    [BUGFIX] Show FlexForm again in TYPO3 13 for Pi1

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,8 @@
 <?php
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
@@ -16,3 +19,18 @@ ExtensionUtility::registerPlugin(
     'LLL:EXT:lux/Resources/Private/Language/locallang_db.xlf:pi1.description',
     'FILE:EXT:lux/Configuration/FlexForms/FlexFormPi1.xml'
 );
+
+// Todo: Remove once TYPO3 13 support is dropped
+if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() === 13) {
+    ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:lux/Configuration/FlexForms/FlexFormPi1.xml',
+        'lux_pi1'
+    );
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        'pi_flexform',
+        'lux_pi1',
+        'after:subheader'
+    );
+}
